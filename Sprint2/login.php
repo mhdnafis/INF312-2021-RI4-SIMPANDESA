944fd4ab6a7fdf11ea4500dee5a6c430cfad69e1
mhdnafis <82092797+mhdnafis@users.noreply.github.com>
2021-06-03 02:28:20 +0700

    Update login.php

--- Sprint2/login.php
+++ Sprint2/login.php
@@ -1,57 +1,104 @@
-<!DOCTYPE html>
+<?php 
+session_start();
+
+require 'koneksi.php';
+
+if (isset($_POST['submit'])) {
+    $nik = $_POST['nik'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM user WHERE nik = '$nik'";
+    $query = mysqli_query($koneksi, $sql);
+    $result = mysqli_fetch_assoc($query);
+    if ($result) {
+        if ($result['pass'] == $password) {
+            
+            $_SESSION["username"] = $result['nama'];
+            $_SESSION["id_user"] = $result['id_user'];
+            $_SESSION["hak_akses"] = $result['hak_akses'];
+            $_SESSION["login"] = "true";
+
+            if ($result['hak_akses'] == "admin") {
+                header('location: admin.php');
+            } else {
+                header('location: index.php');
+            }
+            
+        } else {
+            echo "<script>alert('Password Salah')</script>";
+        }
+    } else {
+        echo "<script>alert('Tidak ada user dengan nik tersebut')</script>";
+    }
+}
+
+?>
+
+<!doctype html>
 <html lang="en">
-  <head>
-    
+
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-
-    <link rel="icon" href="assets/img/SD.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Login</title>
-  </head>
+    <script src="https://kit.fontawesome.com/f9069f9455.js" crossorigin="anonymous"></script>
 
-  <body class="container" style="background-color:#38728B">
-    
-    <div class="jumbotron text-center">
-    <h1 class="body fw-bold text-white">SIMPAN DESA</h1>   
-    <p class="paragraph text-white">Sistem Informasi Peminjaman<br>Peralatan Desa</p>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+</head>
+
+<body class="d-flex align-items-center" style="background: #38728B; height: 100vh; width: 100%">
+
+    <div class="content text-white" style="width:100%">
+        <h1 class="text-center" style="letter-spacing: 0.1em;">SIMPAN DESA</h1>
+        <p class="text-center">Sistem Informasi Peminjaman Peralatan Desa</p>
+        <div class="col-md-6 m-auto">
+            <?php if (isset($_GET['status'])) : ?>
+            <?php if ($_GET['status'] == "registered") : ?>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Selamat!</strong> Akun anda telah terdaftar! silahkan login.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php endif; ?>
+            <?php endif; ?>
+            <hr>
+            <form action="" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-lg" name="nik" id="nik"
+                        placeholder="NIK (Nomor Induk Kependudukan)">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control form-control-lg" name="password" id="password"
+                        placeholder="Password">
+                </div>
+                <hr>
+                <center>
+                    <button type="submit" name="submit" class="btn" style="width: 100px">Login</button>
+                    <br>
+                    <br>
+                    <small class="mt-3">Belum Punya Akun ? <a class="text-warning"
+                            href="register.php">Daftar</a></small>
+                </center>
+
+
+            </form>
+        </div>
     </div>
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 col-lg-4">
-                <div class="login-wrap p-0">
-                    <form action="cek_login.php" class="signin-form" method="post">
-                        <div class="form-group">
-                            <label for="nik"></label>
-                            <input type="text" name="nik" id="nik" class="form-control" placeholder="NIK" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="pass"></label>
-                            <input id="password-field" type="password" name="pass" id="pass" class="form-control" placeholder="Password" required>
-                        </div>
-                        <?php 
-                            if(isset($_GET['pesan'])){
-                                if($_GET['pesan']=="gagal"){
-                                    echo "<div class='alert text-white'>Username dan Password tidak sesuai !</div>";
-                                }
-                            }
-                        ?>
-                        <div class="tombol btn-group justify-content-center">
-                            <button type="submit" name="masuk" class="margin fw-bold form-control btn-white btn-lg mt-5">Masuk</button>
-                        </div>
-                    </form>
-                    <div class="justify-content-center text-center mt-3">
-                       <p class="text-white"> Belum punya akun? <a href="register.php" style="color: #8ba7ff" class="text-decoration-underline">daftar</a> </p>
-                    </div>
-                </div>
-            </div>
-        </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
-  </body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
